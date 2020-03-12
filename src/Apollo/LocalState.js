4b049bad97e5e7b65819519afa1b3f8a545a04c3
hamin <ggamini7@gmail.com>
2020-03-12 23:40:59 +0900

    4.3. Apollo Client

---
+++ src/Apollo/LocalState.js
@@ -0,0 +1,23 @@
+export const defaults = {
+    // token여부를 보고 true false 구분.
+    isLoggedIn: localStorage.getItem("token") !== null ? true : false
+  };
+  
+  export const resolvers = {
+    Mutation: {
+      logUserIn: (_, { token }, { cache }) => {
+        localStorage.setItem("token", token);
+        cache.writeData({
+          data: {
+            isLoggedIn: true
+          }
+        });
+        return null;
+      },
+      logUserOut: (_, __, { cache }) => {
+        localStorage.removeItem("token");
+        window.location.reload();
+        return null;
+      }
+    }
+  };
