d8ad9472b647b7cf8f18bdc8d50c7af89dea565f
hamin <ggamini7@gmail.com>
2020-03-27 01:17:54 +0900

    toggleLike on Post Component

--- src/Components/Post/PostContainer.js
+++ src/Components/Post/PostContainer.js
@@ -18,6 +18,12 @@ const PostContainer = ({
   const [likeCountS, setLikeCount] = useState(likeCount);
   const [currentItem, setCurrentItem] = useState(0);
   const comment = useInput("");
+  const toggleLikeMutation = useMutation(TOGGLE_LIKE, {
+    variables: { postId: id }
+  });
+  const addCommentMutation = useMutation(ADD_COMMENT, {
+    variables: { postId: id, text: comment.value }
+  });
   const slide = () => {
     const totalFiles = files.length;
     if (currentItem === totalFiles - 1) {
@@ -29,6 +35,18 @@ const PostContainer = ({
   useEffect(() => {
     slide();
   }, [currentItem]);
+  
+  const toggleLike = () => {
+    toggleLikeMutation();
+    if (isLikedS === true) {
+      setIsLiked(false);
+      setLikeCount(likeCountS - 1);
+    } else {
+      setIsLiked(true);
+      setLikeCount(likeCountS + 1);
+    }
+  };
+  
   return (
     <PostPresenter
       user={user}
@@ -43,6 +61,7 @@ const PostContainer = ({
       setIsLiked={setIsLiked}
       setLikeCount={setLikeCount}
       currentItem={currentItem}
+      toggleLike={toggleLike}
     />
   );
 };
